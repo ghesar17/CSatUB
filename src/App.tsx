a24f3c1218a989afe09b3af452ab1f94e8bd8625
Ghesar <ghesar4phanday@gmail.com>
2023-07-17 17:45:21 -0400

    added grid for nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import React from 'react'
 import Navbar from "./components/Navbar.tsx";
 
 const App = () => {
+
     return (
         <>
-            <body className="vh-100 background:linear-gradient(176deg,rgb(18,24,27) 50%,rgb(32,39,55) 100%">
-
-            <div>
+            {/*<body className="vh-100 background:linear-gradient(176deg,rgb(18,24,27) 50%,rgb(32,39,55) 100%">*/}
             <Navbar/>
-            <h1 className="text-3xl font-bold underline">Hello World!</h1>
-            </div>
-            </body>
             </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
+import {ChakraBaseProvider} from "@chakra-ui/react";
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+      <ChakraBaseProvider>
     <App />
+      </ChakraBaseProvider>
   </React.StrictMode>,
 )
